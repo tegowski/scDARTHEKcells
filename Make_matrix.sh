140645aca4fcd799e5b9012d5ca392237c48e611
tegowski <66971141+tegowski@users.noreply.github.com>
2022-05-09 10:31:07 -0400

    Delete Make_matrix.sh

--- Make_matrix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -a 1-1254
-
-BAMS=$WORKDIR/singlebams
-file=$(ls WT*dupMarked.bam | sed -n ${SLURM_ARRAY_TASK_ID}p)
-STEM=$(basename "$file" .dupmarked.bam)
-mkdir $WORKDIR/matrix
-
-perl $WORKDIR/software/parseBAM.pl -i $file -o $WORKDIR/matrix/$STEM.matrix --minCoverage 3 --verbose --removeDuplicates
