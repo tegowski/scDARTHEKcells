5615e6d9aa7edffbcf69854a3c82543d1c620f2d
tegowski <66971141+tegowski@users.noreply.github.com>
2022-06-25 15:48:19 -0400

    Create MakematrixYTH.sh

---
+++ MakematrixYTH.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -p scavenger
+#SBATCH -c 8
+#SBATCH --mem=60G
+#SBATCH -a 1-301
+
+
+file=$(ls WT*Aligned.sortedByCoord.out.bam | sed -n ${SLURM_ARRAY_TASK_ID}p)
+STEM=$(basename "$file" .Aligned.sortedByCoord.out.bam)
+
+echo "$[SLURM_ARRAY_TASK_ID]"
+echo "Starting File: $file"
+
+#First, use samtools to remove optical duplicates
+samtools sort -n -o $STEM.name.bam $file
+samtools fixmate -r -m $STEM.name.bam $STEM.fix.bam
+samtools sort -o $STEM.possort.bam $STEM.fix.bam
+samtools markdup -r $STEM.possort.bam $STEM.duprm.bam
+
+rm $STEM.name.bam $STEM.fix.bam $STEM.possort.bam
+
+#Then use Bullseye (parseBAM.pl) to make nucleotide matrix
+WORKDIR="/your/path/here"
+BAMS=$WORKDIR/singlebams
+file=$(ls WT*dupMarked.bam | sed -n ${SLURM_ARRAY_TASK_ID}p)
+STEM=$(basename "$file" .dupmarked.bam)
+mkdir $WORKDIR/matrix
+
+perl $WORKDIR/software/parseBAM.pl \
+-i $file \
+-o $WORKDIR/matrix/$STEM.matrix \
+--minCoverage 5 \
+--verbose \
+--removeDuplicates
