5d7f0c1ee3cc1530b09794c2b18199775bc9b9de
tegowski <66971141+tegowski@users.noreply.github.com>
2022-06-27 10:35:11 -0400

    Update StarBullseyealignMUT.sh

--- StarBullseyealignMUT.sh
+++ StarBullseyealignMUT.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
-#SBATCH -a 1-300
-#SBATCH -c 12
 #SBATCH --mem=60G
+#SBATCH -c 12
+#SBATCH -p scavenger
 
-#Define path to your working directory
-WORKDIR="/work/mrt41/Ex96_SMARTseq/test/"
+#Merge all MUT bamfiles
+samtools merge MUTCellsmerged.bam MUT*duprm.bam
 
-file=$(ls MUTCell*_1.fastq | sed -n ${SLURM_ARRAY_TASK_ID}p) ## this is done for paired end libraries ending with _1 and _2 for each pair.
-STEM=$(basename "$file" _1.fastq)
+#Then use Bullseye (parseBAM.pl) to make nucleotide matrix
+WORKDIR="/your/path/here/"
+BAMS=$WORKDIR/singlebams
+STEM=$(basename "$file" Aligned.sortedByCoord.out.bam.duprm.bam)
+mkdir $WORKDIR/matrix
 
-BAM=$WORKDIR/singlebams
-mkdir -p $BAM
-
-echo "aligning $STEM"
-
-STAR --runMode alignReads \
---genomeDir $WORKDIR/STARindex \
---runThreadN 12 \
---readFilesIn ${STEM}_1.fastq ${STEM}_2.fastq \
---outSAMtype BAM SortedByCoordinate \
---outFilterMismatchNoverReadLmax 0.06 \
---outFileNamePrefix $BAM/${STEM} \
-
-#This next steps marks PCR duplicates in the BAM file so they can be ignored during the next step
-STAR --runMode inputAlignmentsFromBAM \
---runThreadN 12 \
---inputBAMfile $BAM/${STEM}.Aligned.sortedByCoord.out.bam \
---bamRemoveDuplicatesType UniqueIdentical \
---outFileNamePrefix $BAM/${STEM}.dupMarked.bam
+perl $WORKDIR/software/parseBAM.pl \
+-i MUTCellsmerged.bam \
+-o $WORKDIR/matrix/MUTCellsmerged.matrix \
+--minCoverage 5 \
+--verbose \
+--removeDuplicates
