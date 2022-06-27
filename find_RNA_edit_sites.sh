dcff6f00c62345d046af72110fa5f240eb4a2eda
tegowski <66971141+tegowski@users.noreply.github.com>
2022-06-27 10:32:15 -0400

    Update find_RNA_edit_sites.sh

--- find_RNA_edit_sites.sh
+++ find_RNA_edit_sites.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+#SBATCH --mem=24G
+#SBATCH -p scavenger
+#SBATCH -c 8
+#SBATCH -a 1-301
 
-perl $WORKDIR/software/parseBAM.pl --input APOYTHmutmerge.bam --output $WORKDIR/matrix/APOYTHmut.matrix --verbose --removeDuplicates --minCoverage 3
+annotation_file=/datacommons/meyerlab/userdata/matt_tego/gtf/HG38/hg38refFlat_nochr.gtf
+WORKDIR="/your/path/here/"
+bed=$WORKDIR/bed
+file=$(ls WT*matrix.gz | sed -n ${SLURM_ARRAY_TASK_ID}p)
+STEM=$(basename "$file" .matrix.gz)
+mkdir -p $bed
+
+perl $WORKDIR/software/Find_edit_site.pl \
+        --annotationFile $annotation_file \
+        --EditedMatrix $file \
+        --controlMatrix MUTCellsmerged.matrix.gz \
+        --editType C2U \
+        --minEdit 10 \
+        --maxEdit 95 \
+        --editFoldThreshold 1.5 \
+        --EditedMinCoverage 20 \
+        --ControlMinCoverage 10 \
+        --MinEditSites 2 \
+        --cpu 8 \
+        --outfile $bed/$STEM.bed \
+        --bed6 \
+        --verbose
